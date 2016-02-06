00b867ca9c87d5b3c997ca8e76d8c0669c6ccb83
Tanyoung <tanyoung.kim@nokia.com>
2016-02-06 14:01:59 -0800

    Better place looking logic, duplicates finding

--- app/scripts/controllers/setup.js
+++ app/scripts/controllers/setup.js
@@ -10,25 +10,45 @@ angular.module('365daysApp').controller('SetupCtrl', [
         $scope.completedStepIndex = 0;
         $scope.steps = [
             { label : 'year', title: 'Select a year', style: '', result: '' },
+            { title: 'Merge places with a same name', style: '', result: '' },
             { label : 'home', title: 'Select home(s)', style: 'inactive', result: '' },
             { label : 'work', title: 'Select work(s)', style: 'inactive', result: '' },
             { label : 'others', title: 'Select other places', style: 'inactive', result: '' }
         ];
-        $scope.validYear = [];
-        $scope.candidates = {}; //home, work, and other places
-        $scope.selected = {}; //selected candidates' index
 
-        var data = null; //JSON object
-        var places = {}; //selected home and work IDs
+        //selected home and work IDs
+        $scope.validYear = [];
+        var yearNum = 3; //number of years to check
+        var fileNum = 0;
+
+        //places
+        $scope.selected = {}; //selected candidates' index, used in checkbox ng-model
+        var places = []; //selected place IDs, used for exceptions in calculation places of next steps
+        $scope.candidates = {}; //home, work, and other places
+
+        //place name edit
         $scope.isLocationEditCollapsed = true;
         $scope.newLocationNames = [];
+
         //map
         $scope.map = { center: { lat: 37, lng: -122, zoom: 10 } };
 
-        //selected home and work IDs
-        //number of years to check
-        var yearNum = 3;
-        var fileNum = 0;
+        /***
+        **** update steps
+        ****/
+
+        function updateStep(stepIndex, result) {
+            $scope.completedStepIndex = stepIndex + 1;
+            $scope.steps[stepIndex].style = 'done';
+            $scope.steps[stepIndex].result = result;
+            if (stepIndex < $scope.steps.length - 1) {
+                $scope.steps[stepIndex + 1].style = '';
+            }
+        }
+
+        /***
+        **** years at the first steps
+        ****/
 
         function checkYearCount() {
             if (fileNum === yearNum) {
@@ -46,22 +66,32 @@ angular.module('365daysApp').controller('SetupCtrl', [
             checkYearCount();
         }
 
-        var thisYear = moment().year();
-
         function getUrl(year) {
             return 'data/places/places_' + year + '.json';
         }
 
         _.each(_.range(yearNum), function (i) {
-            $http.get(getUrl(thisYear - i))
+            $http.get(getUrl(moment().year() - i))
                 .then(addYears, addCount);
         });
+
+        /***
+        **** merge duplicate
+        ****/
+
+        function mergeDuplicates(duplicates) {
+            console.log(duplicates);
+        }
+
+        /***
+        **** places at the later steps
+        ****/
 
         function getCandidates(type) {
 
             //get candidates excluding previously selected places
-            var selectedIds = _.flatten(_.values(places));
-            $scope.candidates[type] = analyzer.getPlaces(type, data, selectedIds);
+            var selectedIds = _.values(places);
+            $scope.candidates[type] = analyzer.getPlaces(type, selectedIds);
             console.log(type, $scope.candidates[type]);
             var count = $scope.candidates[type].length;
             $scope.selected[type]  = _.map(_.range(count), function (i) {
@@ -83,21 +113,21 @@ angular.module('365daysApp').controller('SetupCtrl', [
             $scope.map.center = { lat: markers[0].lat, lng: markers[0].lng, zoom: 10 };
         }
 
-        function updateStep(stepIndex, result) {
-            $scope.completedStepIndex = stepIndex + 1;
-            $scope.steps[stepIndex].style = 'done';
-            $scope.steps[stepIndex].result = result;
-            if (stepIndex < $scope.steps.length - 1) {
-                $scope.steps[stepIndex + 1].style = '';
-            }
-        }
+        /***
+        **** control from HTML
+        ****/
 
         //step 1: load the data JSON file
         $scope.loadFile = function (year) {
             updateStep(0, year);
             $http.get(getUrl(year)).then(function (d) {
-                data = d.data;
-                getCandidates('home');
+                var duplicates = analyzer.getPlaceList(d.data);
+                if (duplicates.length > 0) {
+                    mergeDuplicates(duplicates);
+                } else {
+                    updateStep(1, 'not needed');
+                    getCandidates('home');
+                }
             });
         };
 
@@ -105,16 +135,15 @@ angular.module('365daysApp').controller('SetupCtrl', [
         $scope.completeStep = function (stepIndex) {
             var lastStep = $scope.steps[stepIndex].label;
 
-            //get place IDs at the end of each step
+            //get place IDs, results of text for html dislpay at the end of each step
             var results = '';
-            places[lastStep] = _.compact(_.map($scope.selected[lastStep], function (d, i) {
+            _.each($scope.selected[lastStep], function (d, i) {
                 if (d) {
                     results = results + $scope.candidates[lastStep][i].name + ', ';
-                    return $scope.candidates[lastStep][i].id;
+                    places.push($scope.candidates[lastStep][i].id);
                 }
-            }));
-            //remove last ', '
-            updateStep(stepIndex, results.slice(0, -2));
+            });
+            updateStep(stepIndex, results.slice(0, -2)); //remove last ', '
 
             //get candidates of next places
             if (stepIndex < $scope.steps.length - 1) {
@@ -134,15 +163,13 @@ angular.module('365daysApp').controller('SetupCtrl', [
         $scope.updateLocationName = function (i, type) {
             $scope.candidates[type][i].name = $scope.newLocationNames[i];
             $scope.newLocationNames[i] = '';
-            // $scope.isLocationEditCollapsed = true;
-            //console.log($scope.candidates[type], $scope.isLocationEditCollapsed);
         };
         $scope.clearLocationName = function (i) {
             $scope.newLocationNames[i] = '';
         };
 
         //testing
-        //$scope.loadFile(2015);
+        $scope.loadFile(2015);
 
     }
 ]);
--- app/scripts/services/analyzer.js
+++ app/scripts/services/analyzer.js
@@ -1,22 +1,22 @@
 'use strict';
 
-angular.module('365daysApp').service('analyzer', [
+angular.module('365daysApp').factory('analyzer', [
     '_',
     'moment',
     function (_, moment) {
 
-    /* time convert functions */
-    function getDuration(st, et, trim, currentDate) {
+    //list of all places
+    var allPlaces = [];
+
+    function getDuration(st, et, currentDate) {
 
         //trim if the segment starts from the previous day or end on the next day
-        if (trim) {
-            var sDate = +st.substring(0, 8);
-            var eDate = +et.substring(0, 8);
-            if (+currentDate > sDate) {
-                st = currentDate + 'T000000Z';
-            } else if (eDate > +currentDate) {
-                et = currentDate + 'T000000Z';
-            }
+        var sDate = +st.substring(0, 8);
+        var eDate = +et.substring(0, 8);
+        if (+currentDate > sDate) {
+            st = currentDate + 'T000000Z';
+        } else if (eDate > +currentDate) {
+            et = currentDate + 'T235959Z';
         }
         return moment(et, 'YYYYMMDDThhmmssZ').diff(moment(st, 'YYYYMMDDThhmmssZ'), 'minutes');
     }
@@ -28,72 +28,77 @@ angular.module('365daysApp').service('analyzer', [
             minute + ' minute' + (minute > 1 ? 's ' : ' ');
     }
 
-    function orderPlaces(placesByDay) {
-
-        //order places by total time stpend, object by place id
-        return _.map(_.sortBy(_.map(_.groupBy(placesByDay, function (day) {
-                return day.id;
-            }), function (placeList) {
-                return {
-                    id: placeList[0].id,
-                    name: placeList[0].name,
-                    location: placeList[0].location,
-                    totalMinute: _.reduce(_.pluck(placeList, 'duration'), function (memo, num) {
-                        return memo + num;
-                    }, 0),
-                    count: _.size(placeList)
-                };
-            }), function (place) {
-                return place.totalMinute;
-            }).reverse().slice(0, 10), function (place) {
-                place.duration = toHourMinute(place.totalMinute);
-                delete place.totalMinute;
-                return place;
-            });
+    function sum(list) {
+        return _.reduce(list, function (memo, num) {
+                return memo + num;
+            }, 0);
     }
 
-    function getPlaceElements(p, trim, date) {
-        return {
-            id: p.place.id,
-            name: p.place.name ?
-                p.place.name :
-                p.place.type,
-            location: { lat: p.place.location.lat, lng: p.place.location.lon },
-            duration: getDuration(p.startTime, p.endTime, trim, date)
-        };
+    function groupPlacesById(list) {
+        return _.map(_.groupBy(list, function (p) {
+            return p.id;
+        }), function (g, id) {
+            return {
+                id: id,
+                name: g[0].name,
+                location: g[0].location,
+                duration: sum(_.pluck(g, 'duration')),
+                atMidnight: sum(_.pluck(g, 'atMidnight')),
+                //count is always 1 when group by day
+                count: g[0].count ? sum(_.pluck(g, 'count')) : 1
+            };
+        });
     }
 
-    function getHome(dayList) {
-
-        var placesByDay = _.compact(_.map(dayList, function (day) {
-            //first place of the day - present at midnight
-            if (!_.isNull(day.segments) && day.date !== day.segments[0].startTime.substring(0, 8)) {
-                var firstPlace = day.segments[0];
-                return getPlaceElements(firstPlace, false);
-            }
-        }));
-        return orderPlaces(placesByDay);
-    }
-
-    function getAllPlaces(dayList, exceptions) {
-        return _.flatten(_.compact(_.map(dayList, function (day) {
+    this.getPlaceList = function (data) {
+        var placesGroupedByDay = _.flatten(_.compact(_.map(data, function (day) {
                 if (!_.isNull(day.segments)) {
-                    return _.compact(_.map(day.segments, function (seg) {
-                        if (!_.contains(exceptions, seg.place.id)) {
-                            return getPlaceElements(seg, true, day.date);
-                        }
+                    var placesInDay = _.compact(_.map(day.segments, function (seg) {
+                        return {
+                            id: seg.place.id,
+                            name: seg.place.name ? seg.place.name : seg.place.type,
+                            location: { lat: seg.place.location.lat, lng: seg.place.location.lon },
+                            duration: getDuration(seg.startTime, seg.endTime, day.date),
+                            atMidnight: day.date !== seg.startTime.substring(0, 8) ? 1 : 0,
+                        };
                     }));
+                    return groupPlacesById(placesInDay);
                 }
             })));
-    }
 
-    this.getPlaces = function (type, data, exceptions) {
-        if (type === 'home') {
-            return getHome(data);
-        } else {
-            var allPlaces = getAllPlaces(data, exceptions);
-            return orderPlaces(allPlaces);
-        }
+        var all = groupPlacesById(placesGroupedByDay);
+        allPlaces = all;
+
+        //Check same names with different IDs
+        var duplicates = _.compact(_.map(_.omit(_.groupBy(all, function (d) {
+            return d.name;
+        }), 'unknown'), function (d, key) {
+            if (d.length > 1) {
+                return {
+                    name: key,
+                    location: _.pluck(d, 'location')
+                };
+            }
+        }));
+
+        return duplicates;
+
+    };
+
+    this.getPlaces = function (type, exceptions) {
+
+        return _.map(_.sortBy(_.filter(allPlaces, function (place) {
+                return !_.contains(exceptions, place.id);
+            }), function (place) {
+                if (type === 'home') {
+                    return place.atMidnight;
+                } else {
+                    return place.duration;
+                }
+            }).reverse().slice(0, 10), function (place) {
+                place.duration = toHourMinute(place.duration);
+                return place;
+            });
     };
 
     return this;
